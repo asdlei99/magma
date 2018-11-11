83caa0963e2c91d1210b7ebaa4e4641aa51a54ac
Victor Coda <victor.coda@gmail.com>
2018-11-12 01:12:43 +0200

    Avoid nested class definitions

--- auxiliary/managedDescriptorSet.h
+++ auxiliary/managedDescriptorSet.h
@@ -38,31 +38,10 @@ namespace magma
     {
         /* */
 
-        // TODO: support immutable samplers
         class ManagedDescriptorSet : public sys::NonCopyable
         {
         public:
-            class ShaderStageBindings
-            {
-            public:
-                void bindImageView(uint32_t binding,
-                    std::shared_ptr<const ImageView> imageView,
-                    std::shared_ptr<const Sampler> sampler);
-                void bindBuffer(uint32_t binding,
-                    std::shared_ptr<const Buffer> buffer,
-                    VkDeviceSize offset = 0,
-                    VkDeviceSize range = VK_WHOLE_SIZE);
-                void bindTexelBufferView(uint32_t binding,
-                    std::shared_ptr<const BufferView> texelBufferView);
-
-            private:
-                std::unordered_map<uint32_t, VkWriteDescriptorSet> descriptorWrites; // Should be unique descriptor for each binding!
-                std::list<VkDescriptorImageInfo> imageInfos;
-                std::list<VkDescriptorBufferInfo> bufferInfos;
-                std::list<VkBufferView> bufferViews;
-                bool updated = false;
-                friend ManagedDescriptorSet;
-            };
+            class ShaderStageBindings;
 
         public:
             ManagedDescriptorSet(std::shared_ptr<Device> device,
@@ -85,6 +64,31 @@ namespace magma
             std::shared_ptr<DescriptorSetLayout> layout;
             std::shared_ptr<DescriptorSet> set;
             std::unordered_map<VkShaderStageFlagBits, ShaderStageBindings> stages;
+        };
+
+        /* */
+
+        // TODO: support immutable samplers
+        class ManagedDescriptorSet::ShaderStageBindings
+        {
+        public:
+            void bindImageView(uint32_t binding,
+                std::shared_ptr<const ImageView> imageView,
+                std::shared_ptr<const Sampler> sampler);
+            void bindBuffer(uint32_t binding,
+                std::shared_ptr<const Buffer> buffer,
+                VkDeviceSize offset = 0,
+                VkDeviceSize range = VK_WHOLE_SIZE);
+            void bindTexelBufferView(uint32_t binding,
+                std::shared_ptr<const BufferView> texelBufferView);
+
+        private:
+            std::unordered_map<uint32_t, VkWriteDescriptorSet> descriptorWrites;
+            std::list<VkDescriptorImageInfo> imageInfos;
+            std::list<VkDescriptorBufferInfo> bufferInfos;
+            std::list<VkBufferView> bufferViews;
+            bool updated = false;
+            friend ManagedDescriptorSet;
         };
     } // namespace aux
 } // namespace magma
