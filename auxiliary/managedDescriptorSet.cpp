3837fb958bc8ea8b82ca84a808708a9c18871b84
Victor Coda <victor.coda@gmail.com>
2018-11-12 00:33:06 +0200

    Draft implementation of ManagedDescriptorSet object. All resources are binded to ShaderStageBindings object. In order to get valid layout and descriptor set, ::finalize() method should be called.

--- auxiliary/managedDescriptorSet.cpp
+++ auxiliary/managedDescriptorSet.cpp
@@ -16,49 +16,183 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
+#include <unordered_set>
 #include "managedDescriptorSet.h"
+#include "../objects/device.h"
+#include "../objects/buffer.h"
+#include "../objects/bufferView.h"
+#include "../objects/image.h"
+#include "../objects/imageView.h"
+#include "../objects/sampler.h"
+#include "../objects/descriptorPool.h"
+#include "../objects/descriptorSetLayout.h"
+#include "../objects/descriptorSet.h"
+#include "../helpers/stackArray.h"
+#include "../misc/exception.h"
 
 namespace magma
 {
 namespace aux
 {
-ManagedDescriptorSet::ManagedDescriptorSet()
+void ManagedDescriptorSet::ShaderStageBindings::bindImageView(uint32_t binding, std::shared_ptr<const ImageView> imageView, std::shared_ptr<const Sampler> sampler)
 {
-    // TODO:
-}
-
-void ManagedDescriptorSet::bindBuffer(uint32_t binding, std::shared_ptr<const Buffer> buffer, VkShaderStageFlagBits shaderStages)
-{
+    VkDescriptorImageInfo imageInfo;
+    imageInfo.sampler = *sampler;
+    imageInfo.imageView = *imageView;
+    imageInfo.imageLayout = imageView->getImage()->getLayout();
+    imageInfos.push_back(imageInfo);
+    VkWriteDescriptorSet writeDescriptor;
+    writeDescriptor.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    writeDescriptor.pNext = nullptr;
+    writeDescriptor.dstSet = VK_NULL_HANDLE;
+    writeDescriptor.dstBinding = binding;
+    writeDescriptor.dstArrayElement = 0;
+    writeDescriptor.descriptorCount = 1;
+    writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    writeDescriptor.pImageInfo = &imageInfos.back();
+    writeDescriptor.pBufferInfo = nullptr;
+    writeDescriptor.pTexelBufferView = nullptr;
+    descriptorWrites[binding] = writeDescriptor;
     updated = true;
 }
 
-void ManagedDescriptorSet::bindImageView(uint32_t binding, std::shared_ptr<const ImageView> imageView, std::shared_ptr<const Sampler> sampler,
-    VkShaderStageFlagBits shaderStages)
+void ManagedDescriptorSet::ShaderStageBindings::bindBuffer(uint32_t binding, std::shared_ptr<const Buffer> buffer,
+    VkDeviceSize offset /* 0 */, VkDeviceSize range /* VK_WHOLE_SIZE */)
 {
-    updated = true;
-}
-
-void ManagedDescriptorSet::bindTexelBufferView(uint32_t binding, std::shared_ptr<const BufferView> texelBufferView, VkShaderStageFlagBits shaderStages)
-{
-    updated = true;
-}
-
-std::shared_ptr<DescriptorSet> ManagedDescriptorSet::getSet()
-{
-    if (updated)
+    VkDescriptorBufferInfo bufferInfo;
+    bufferInfo.buffer = *buffer;
+    bufferInfo.offset = offset;
+    bufferInfo.range = range;
+    bufferInfos.push_back(bufferInfo);
+    VkWriteDescriptorSet writeDescriptor;
+    writeDescriptor.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    writeDescriptor.pNext = nullptr;
+    writeDescriptor.dstSet = VK_NULL_HANDLE;
+    writeDescriptor.dstBinding = binding;
+    writeDescriptor.dstArrayElement = 0;
+    writeDescriptor.descriptorCount = 1;
+    switch (buffer->getUsage())
     {
-        // TODO: rebuild
+    case VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT:
+        writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER;
+        break;
+    case VK_BUFFER_USAGE_STORAGE_TEXEL_BUFFER_BIT:
+        writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER;
+        break;
+    case VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT:
+        writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        break;
+    case VK_BUFFER_USAGE_STORAGE_BUFFER_BIT:
+        writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        break;
+    //VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC?
+    //VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC?
+    default:
+        MAGMA_THROW("unknown buffer usage");
     }
-    return set;
+    writeDescriptor.pImageInfo = nullptr;
+    writeDescriptor.pBufferInfo = &bufferInfos.back();
+    writeDescriptor.pTexelBufferView = nullptr;
+    descriptorWrites[binding] = writeDescriptor;
+    updated = true;
+}
+
+void ManagedDescriptorSet::ShaderStageBindings::bindTexelBufferView(uint32_t binding, std::shared_ptr<const BufferView> texelBufferView)
+{
+    bufferViews.push_back(*texelBufferView);
+    VkWriteDescriptorSet writeDescriptor;
+    writeDescriptor.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    writeDescriptor.pNext = nullptr;
+    writeDescriptor.dstSet = VK_NULL_HANDLE;
+    writeDescriptor.dstBinding = binding;
+    writeDescriptor.dstArrayElement = 0;
+    writeDescriptor.descriptorCount = 1;
+    writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER;
+    writeDescriptor.pImageInfo = nullptr;
+    writeDescriptor.pBufferInfo = nullptr;
+    writeDescriptor.pTexelBufferView = &bufferViews.back();
+    descriptorWrites[binding] = writeDescriptor;
+    updated = true;
+}
+
+ManagedDescriptorSet::ManagedDescriptorSet(std::shared_ptr<Device> device,
+    std::shared_ptr<DescriptorPool> pool /* nullptr */,
+    std::shared_ptr<IAllocator> allocator /* nullptr */):
+    device(device),
+    pool(pool),
+    allocator(allocator)
+{}
+
+void ManagedDescriptorSet::finalize()
+{
+    uint32_t numDescriptorWrites = 0;
+    if (!pool)
+    {
+        std::unordered_map<VkDescriptorType, uint32_t> descriptorCounts;
+        for (const auto& stage : stages)
+        {   // Count number of descriptors of corresponding type
+            for (const auto& write : stage.second.descriptorWrites)
+            {
+                const VkWriteDescriptorSet& writeDescriptor = write.second;
+                descriptorCounts[writeDescriptor.descriptorType] += writeDescriptor.descriptorCount;
+                ++numDescriptorWrites;
+            }
+        }
+        // If pool not provided, create our own
+        std::vector<magma::Descriptor> descriptors;
+        for (const auto& counts : descriptorCounts)
+        {
+            const magma::Descriptor descriptor(counts.first /* type */, counts.second /* count */);
+            descriptors.push_back(descriptor);
+        }
+        pool = std::make_shared<magma::DescriptorPool>(device, 1, descriptors, false, allocator);
+    }
+    // Prepare layout bindings
+    std::vector<DescriptorSetLayout::Binding> bindings;
+    std::unordered_set<uint32_t> uniqueBindings;
+    bindings.reserve(numDescriptorWrites);
+    for (const auto& stage : stages)
+    {
+        for (const auto& write : stage.second.descriptorWrites)
+        {
+            const VkWriteDescriptorSet& writeDescriptor = write.second;
+            if (uniqueBindings.find(writeDescriptor.dstBinding) != uniqueBindings.end())
+                MAGMA_THROW("descriptor binding should be unique across pipeline");
+            uniqueBindings.insert(writeDescriptor.dstBinding);
+            const Descriptor descriptor(writeDescriptor.descriptorType, writeDescriptor.descriptorCount);
+            const VkShaderStageFlagBits stageFlags = stage.first; // TODO: combine flags from different stages?
+            bindings.emplace_back(writeDescriptor.dstBinding, descriptor, stageFlags);
+        }
+    }
+    // Create set layout
+    layout = std::make_shared<DescriptorSetLayout>(device, bindings, 0, allocator);
+    // Allocate descriptor set
+    set = pool->allocateDescriptorSet(layout);
+    // Create linear array of descriptor writes to pass to API
+    MAGMA_STACK_ARRAY(VkWriteDescriptorSet, descriptorWrites, numDescriptorWrites);
+    for (auto& stage : stages)
+    {
+        for (const auto& write : stage.second.descriptorWrites)
+        {
+            descriptorWrites.put(write.second);
+            descriptorWrites.last().dstSet = MAGMA_HANDLE(set); // Assing newly created handle
+        }
+    }
+    // Write all descriptors
+    vkUpdateDescriptorSets(MAGMA_HANDLE(device), MAGMA_COUNT(descriptorWrites), descriptorWrites,
+        0, nullptr);
+    for (auto& stage : stages)
+        stage.second.updated = false;
 }
 
 std::shared_ptr<DescriptorSetLayout> ManagedDescriptorSet::getLayout()
 {
-    if (updated)
-    {
-        // TODO: rebuild
-    }
     return layout;
+}
+
+std::shared_ptr<DescriptorSet> ManagedDescriptorSet::getDescriptorSet()
+{
+    return set;
 }
 } // namespace aux
 } // namespace magma
--- auxiliary/managedDescriptorSet.h
+++ auxiliary/managedDescriptorSet.h
@@ -17,11 +17,14 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
 #include <memory>
+#include <list>
+#include <unordered_map>
 #include "../vulkan.h"
 #include "../nonCopyable.h"
 
 namespace magma
 {
+    class Device;
     class Buffer;
     class BufferView;
     class ImageView;
@@ -29,34 +32,60 @@ namespace magma
     class DescriptorSet;
     class DescriptorSetLayout;
     class DescriptorPool;
+    class IAllocator;
 
     namespace aux
     {
         /* */
 
-        // TODO: add immutable samplers
+        // TODO: support immutable samplers
         class ManagedDescriptorSet : public sys::NonCopyable
         {
         public:
-            ManagedDescriptorSet();
-            void bindBuffer(uint32_t binding,
-                std::shared_ptr<const Buffer> rbuffer,
-                VkShaderStageFlagBits shaderStages);
-            void bindImageView(uint32_t binding,
-                std::shared_ptr<const ImageView> imageView,
-                std::shared_ptr<const Sampler> sampler,
-                VkShaderStageFlagBits shaderStages);
-            void bindTexelBufferView(uint32_t binding,
-                std::shared_ptr<const BufferView> texelBufferView,
-                VkShaderStageFlagBits shaderStages);
-            std::shared_ptr<DescriptorSet> getSet();
+            class ShaderStageBindings
+            {
+            public:
+                ShaderStageBindings() {}
+                void bindImageView(uint32_t binding,
+                    std::shared_ptr<const ImageView> imageView,
+                    std::shared_ptr<const Sampler> sampler);
+                void bindBuffer(uint32_t binding,
+                    std::shared_ptr<const Buffer> buffer,
+                    VkDeviceSize offset = 0,
+                    VkDeviceSize range = VK_WHOLE_SIZE);
+                void bindTexelBufferView(uint32_t binding,
+                    std::shared_ptr<const BufferView> texelBufferView);
+
+            private:
+                std::unordered_map<uint32_t, VkWriteDescriptorSet> descriptorWrites; // Should be unique descriptor for each binding!
+                std::list<VkDescriptorImageInfo> imageInfos;
+                std::list<VkDescriptorBufferInfo> bufferInfos;
+                std::list<VkBufferView> bufferViews;
+                bool updated = false;
+                friend ManagedDescriptorSet;
+            };
+
+        public:
+            ManagedDescriptorSet(std::shared_ptr<Device> device,
+                std::shared_ptr<DescriptorPool> pool = nullptr,
+                std::shared_ptr<IAllocator> allocator = nullptr);
+            ShaderStageBindings& vertexStage() { return stages[VK_SHADER_STAGE_VERTEX_BIT]; }
+            ShaderStageBindings& tessControlStage() { return stages[VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT]; }
+            ShaderStageBindings& tessEvaluationStage() { return stages[VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT]; }
+            ShaderStageBindings& geometryStage() { return stages[VK_SHADER_STAGE_GEOMETRY_BIT]; }
+            ShaderStageBindings& fragmentStage() { return stages[VK_SHADER_STAGE_FRAGMENT_BIT]; }
+            ShaderStageBindings& computeStage() { return stages[VK_SHADER_STAGE_COMPUTE_BIT]; }
+            void finalize();
             std::shared_ptr<DescriptorSetLayout> getLayout();
+            std::shared_ptr<DescriptorSet> getDescriptorSet();
 
         private:
-            std::shared_ptr<DescriptorSet> set;
-            std::shared_ptr<DescriptorSetLayout> layout;
+            std::shared_ptr<Device> device;
+            std::shared_ptr<IAllocator> allocator;
             std::shared_ptr<DescriptorPool> pool;
-            bool updated = false;
+            std::shared_ptr<DescriptorSetLayout> layout;
+            std::shared_ptr<DescriptorSet> set;
+            std::unordered_map<VkShaderStageFlagBits, ShaderStageBindings> stages;
         };
     } // namespace aux
 } // namespace magma
